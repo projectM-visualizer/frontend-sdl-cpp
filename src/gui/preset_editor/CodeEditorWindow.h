4c5f609278962b3dec96ed6526f7d2add4100d8b
Kai Blaschke <kai.blaschke@kb-dev.net>
2025-09-11 00:56:21 +0200

    Make code editor child window collapsible
    Toggles drawing of the whole child as interfering with the user-resizable window makes ImGui forget the previously set height when expanding it again.

--- src/gui/preset_editor/CodeEditorWindow.h
+++ src/gui/preset_editor/CodeEditorWindow.h
@@ -20,8 +20,10 @@ public:
     void OpenCodeInTab(ExpressionCodeTypes type, std::string& code, int index);
 
 private:
-    bool _visible{false};
-    std::list<CodeEditorTab> _codeEditorTabs;
+    bool _visible{false}; //!< Determines if the code editor window is visible.
+    bool _collapsed{false}; //!< If true, the window is displayed collapsed, e.g. the tab contents aren't rendered.
+
+    std::list<CodeEditorTab> _codeEditorTabs; //!< Currently opened editor tabs.
 };
 
 } // namespace Editor
