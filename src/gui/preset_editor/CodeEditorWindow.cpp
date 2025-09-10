6900df7fc7f29c70b8ecb2787f305f934689416f
Kai Blaschke <kai.blaschke@kb-dev.net>
2025-09-11 00:56:21 +0200

    Make code editor child window collapsible
    Toggles drawing of the whole child as interfering with the user-resizable window makes ImGui forget the previously set height when expanding it again.

--- src/gui/preset_editor/CodeEditorWindow.cpp
+++ src/gui/preset_editor/CodeEditorWindow.cpp
@@ -1,6 +1,7 @@
 #include "CodeEditorWindow.h"
 
 #include "CodeContextInformation.h"
+#include "IconsFontAwesome7.h"
 
 #include "imgui.h"
 
@@ -15,25 +16,42 @@ void CodeEditorWindow::Draw()
 
     ImGui::PushID("CodeEditorWindow");
 
-    if (ImGui::BeginChild("CodeEditorChild", ImVec2(0, 500), ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeY, ImGuiWindowFlags_None))
+    if (ImGui::Button(_collapsed ? ICON_FA_CARET_RIGHT "##CollapseButton" : ICON_FA_CARET_DOWN "##CollapseButton"))
     {
-        if (ImGui::BeginTabBar("OpenCodeTabs", ImGuiTabBarFlags_AutoSelectNewTabs))
-        {
-            for (auto it = begin(_codeEditorTabs); it != end(_codeEditorTabs);)
-            {
-                if (it->Draw())
-                {
-                    ++it;
-                    continue;
-                }
+        _collapsed = !_collapsed;
+    }
 
-                it = _codeEditorTabs.erase(it);
+    ImGui::SameLine();
+
+    if (!_collapsed)
+    {
+        ImGui::SetNextWindowSize(ImVec2(0, 500), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowBgAlpha(_collapsed ? 0.0f : 0.5f);
+        if (ImGui::BeginChild("CodeEditorChild", ImVec2(0, 500), ImGuiChildFlags_ResizeY, ImGuiWindowFlags_None))
+        {
+            if (ImGui::BeginTabBar("OpenCodeTabs", ImGuiTabBarFlags_AutoSelectNewTabs))
+            {
+                for (auto it = begin(_codeEditorTabs); it != end(_codeEditorTabs);)
+                {
+                    if (it->Draw())
+                    {
+                        ++it;
+                        continue;
+                    }
+
+                    it = _codeEditorTabs.erase(it);
+                }
             }
 
             ImGui::EndTabBar();
         }
+        ImGui::EndChild();
     }
-    ImGui::EndChild();
+    else
+    {
+        ImGui::SameLine();
+        ImGui::TextDisabled("%s", ("(" + std::to_string(_codeEditorTabs.size()) + " Tabs Open)").c_str());
+    }
 
     if (_codeEditorTabs.empty())
     {
@@ -45,6 +63,9 @@ void CodeEditorWindow::Draw()
 
 void CodeEditorWindow::OpenCodeInTab(ExpressionCodeTypes type, std::string& code, int index)
 {
+    // Fold out the editor if the user opens a new tab or focuses an existing one.
+    _collapsed = false;
+
     std::string newTabTitle = CodeContextInformation::GetContextName(type, index);
     for (auto& tab : _codeEditorTabs)
     {
@@ -55,8 +76,12 @@ void CodeEditorWindow::OpenCodeInTab(ExpressionCodeTypes type, std::string& code
         }
     }
 
-    _codeEditorTabs.emplace_back(type, code, index);
+    CodeEditorTab newCodeEditorTab(type, code, index);
+    newCodeEditorTab.SetSelected();
+    _codeEditorTabs.push_back(std::move(newCodeEditorTab));
+
     _visible = true;
 }
+
 
 } // namespace Editor
